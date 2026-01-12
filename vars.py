3e2da33b211f3f1b2b2b2936a87d37afa2cb573d
pj544531-ctrl <pj544531@gmail.com>
2026-01-11 23:39:30 -0800

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,8 +2,8 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", "23231900"))
-API_HASH = os.environ.get("API_HASH", "d030150e323a1ff50851db2044cfe6ad")
+API_ID = int(os.environ.get("API_ID", "25861636"))
+API_HASH = os.environ.get("API_HASH", "7cb06d66a4ad6407aff96477c15d86eb")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8590738996:AAF3RS6hsNT4le8Dkp4qWDjguk4LXQk8n9k")
 
 CREDIT = os.environ.get("CREDIT", "MANISH JOSHI")
@@ -13,7 +13,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://itsgoluAPI:jrMHSipT
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "8463673971"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "5805982370"))
 ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]  # Default to owner ID
 
 # Channel Configuration
@@ -59,6 +59,7 @@ Please contact the admin to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
